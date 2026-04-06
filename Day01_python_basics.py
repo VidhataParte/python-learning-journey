a821e5103c5c7fb029e00900ac61e2a6107ade01
Vidhata Parte <vidhata.parte01@gmail.com>
2026-04-06 08:36:26 +0530

    Add files via upload

---
+++ Day01_python_basics.py
@@ -0,0 +1,31 @@
+# Day 1 - Python Basics Practice
+# Author: Vidhata
+# Goal: Start journey towards Data Analyst
+
+# 1. Basic print statements
+print("Hello World")
+print("This is my first Python program")
+
+# 2. Personal message
+print("Welcome Vidhata to Python programming language")
+
+# 3. Motivation line
+print("Starting one step ahead towards Data Analyst & Data Scientist ")
+
+# 4. Variables (NEW concept)
+name = "Vidhata"
+goal = "Data Analyst"
+
+print("My name is:", name)
+print("My goal is to become a", goal)
+
+# 5. Taking user input 
+user_name = input("Enter your name: ")
+print("Hello", user_name, "! Welcome to Python.")
+
+# 6. Simple calculation 
+num1 = 10
+num2 = 20
+
+sum_result = num1 + num2
+print("Sum of", num1, "and", num2, "is:", sum_result)
